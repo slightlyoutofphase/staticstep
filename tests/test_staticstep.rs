07cb4a1047aa6c7e9713f21b8734134bf689fd66
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2021-03-31 17:02:56 -0400

    Add a couple more tests

--- tests/test_staticstep.rs
+++ tests/test_staticstep.rs
@@ -95,7 +95,36 @@ fn dec_by_inclusive_step_too_big() {
 }
 
 #[test]
-fn dec_by_exclusive_negative_end() {
+fn dec_by_exclusive_negative_end_even_step() {
+  let mut r = (64isize..-64isize).dec_by::<16>();
+  assert_eq!(r.next(), Some(64));
+  assert_eq!(r.next(), Some(48));
+  assert_eq!(r.next(), Some(32));
+  assert_eq!(r.next(), Some(16));
+  assert_eq!(r.next(), Some(0));
+  assert_eq!(r.next(), Some(-16));
+  assert_eq!(r.next(), Some(-32));
+  assert_eq!(r.next(), Some(-48));
+  assert_eq!(r.next(), None);
+}
+
+#[test]
+fn dec_by_inclusive_negative_end_even_step() {
+  let mut r = (64isize..=-64isize).dec_by::<16>();
+  assert_eq!(r.next(), Some(64));
+  assert_eq!(r.next(), Some(48));
+  assert_eq!(r.next(), Some(32));
+  assert_eq!(r.next(), Some(16));
+  assert_eq!(r.next(), Some(0));
+  assert_eq!(r.next(), Some(-16));
+  assert_eq!(r.next(), Some(-32));
+  assert_eq!(r.next(), Some(-48));
+  assert_eq!(r.next(), Some(-64));
+  assert_eq!(r.next(), None);
+}
+
+#[test]
+fn dec_by_exclusive_negative_end_uneven_step() {
   // This should be the same as the inclusive version below, based on how many times
   // the value of `STEP` fits between `self.start` and `self.end` in this case.
   let mut r = (64isize..-64isize).dec_by::<27>();
@@ -108,7 +137,7 @@ fn dec_by_exclusive_negative_end() {
 }
 
 #[test]
-fn dec_by_inclusive_negative_end() {
+fn dec_by_inclusive_negative_end_uneven_step() {
   let mut r = (64isize..=-64isize).dec_by::<27>();
   assert_eq!(r.next(), Some(64));
   assert_eq!(r.next(), Some(37));
